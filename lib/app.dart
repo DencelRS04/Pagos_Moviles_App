4c0a68537c1c47c7dbf98b42e5de5d6161afdd1c
Dencel Rodriguez Solano <denceljrs04@gmail.com>
2026-04-17 10:52:35 -0600

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pagos Móviles',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Center(child: Text('Pagos Móviles'))),
+    );
+  }
+}
